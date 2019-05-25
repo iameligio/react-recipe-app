c310a621bf9528ab1f891c872ade80e7ddb14346
Eligio Tapiador Jr <iameligio@gmail.com>
2019-05-25 21:59:12 +0800

    Route tag component attribute change to lower case

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './pages/Home';
-import Recipes from './pages/Recipes';
-import SingleRecipe from './pages/SingleRecipe';
-import Default from './pages/Default';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import SingleRecipe from "./pages/SingleRecipe";
+import Default from "./pages/Default";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <Home />
-          <Recipes />
-          <SingleRecipe />
-          <Default />
-        </div>
+        <main>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/recipes" exact component={Recipes} />
+            <Route path="/recipes/:id" component={SingleRecipe} />
+            <Route component={Default} />
+          </Switch>
+        </main>
       </Router>
 
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ export default class Home extends Component {
  render() {
   return (
    <h4>
-    Homepage
+    Welcome to Homepage
    </h4>
   )
  }
