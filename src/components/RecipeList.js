39ae98ec00ccf978f9b0fdee2b640919734c9de1
Eligio Tapiador Jr <iameligio@gmail.com>
2019-05-26 09:47:30 +0800

    Functional recipe search and recipe list

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -3,11 +3,23 @@ import Recipe from "./Recipe";
 
 export default class RecipeList extends Component {
  render() {
+
+  const { recipes } = this.props;
+
   return (
-   <div>
-    hello from recipe list
-    <Recipe />
-   </div>
+   <>
+    <div className="container py-5">
+     <div className="row">
+      <div className="col-10 mx-auto col-md-6 text-center text-uppercase mn-3">
+       <h1 className="text-slanted">recipe list</h1>
+      </div>
+     </div>
+     <div className="row">
+      {recipes.map(recipe => (<Recipe key={recipe.recipe_id} recipe={recipe} />))}
+     </div>
+
+    </div>
+   </>
   )
  }
 }
