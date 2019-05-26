4325d279e3d65e2f1b598cf05465ff0df7ad6c38
Eligio Tapiador Jr <iameligio@gmail.com>
2019-05-26 11:05:02 +0800

    Dynamic Single Recipe with ENV variable

--- src/pages/SingleRecipe.js
+++ src/pages/SingleRecipe.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { recipeData } from "../data/tempDetails";
 import { Link } from "react-router-dom";
+import { fileURLToPath } from 'url';
 
 export default class SingleRecipe extends Component {
   constructor(props) {
@@ -8,12 +9,26 @@ export default class SingleRecipe extends Component {
     //console.log(this.props)
     const id = this.props.match.params.id;
     this.state = {
-      recipe: recipeData,
+      //recipe: recipeData,
+      recipe: {},
       id,
-      loading: false
+      loading: true
     }
   }
+  async componentDidMount() {
+    const url = `https://www.food2fork.com/api/get?key=${process.env.REACT_APP_API_KEY}&rId=${this.state.id}`;
 
+    try {
+      const response = await fetch(url);
+      const responseData = await response.json();
+      this.setState({
+        recipe: responseData.recipe,
+        loading: false
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   render() {
     const { image_url, publisher, publisher_url, source_url, title, ingredients } = this.state.recipe;
