251ef93b2aca589699b20d634af6c19349230985
Eligio Tapiador Jr <iameligio@gmail.com>
2019-05-26 11:24:09 +0800

    Dynamic recipes base on food2fork API

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -5,12 +5,30 @@ import { recipeData } from "../data/tempList";
 
 export default class Recipes extends Component {
  constructor(props) {
-  super(props)
+  super(props);
+  this.getRecipes = this.getRecipes.bind(this);
  }
 
  state = {
   recipes: recipeData,
-  search: ''
+  search: '',
+  url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`
+ };
+
+ async getRecipes() {
+  try {
+   const data = await fetch(this.state.url);
+   const jsonData = await data.json();
+   this.setState({
+    recipes: jsonData.recipes
+   });
+  } catch (error) {
+   console.log(error);
+  }
+ }
+
+ componentDidMount() {
+  this.getRecipes();
  }
 
  handleChange = (e) => {
