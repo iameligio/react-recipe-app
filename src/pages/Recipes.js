b179c7720bd4e573cda0236e42870e01b2d2f259
Eligio Tapiador Jr <iameligio@gmail.com>
2019-05-26 11:59:10 +0800

    Working search bar with custom error message

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -12,16 +12,27 @@ export default class Recipes extends Component {
  state = {
   recipes: recipeData,
   search: '',
-  url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`
+  url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`,
+  base_url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`,
+  query: '&q=',
+  error: ''
  };
 
  async getRecipes() {
   try {
    const data = await fetch(this.state.url);
    const jsonData = await data.json();
-   this.setState({
-    recipes: jsonData.recipes
-   });
+   if (jsonData.recipes.length === 0) {
+    this.setState({
+     error: "sorry but your search did not return any recipes, please try again or press search icon for the most popular recipes"
+    });
+   } else {
+    this.setState({
+     recipes: jsonData.recipes,
+     error: ""
+    });
+   }
+
   } catch (error) {
    console.log(error);
   }
@@ -34,11 +45,16 @@ export default class Recipes extends Component {
  handleChange = (e) => {
   this.setState({
    search: e.target.value
-  })
+  }, () => this.getRecipes());
  };
 
  handleSubmit = (e) => {
-  e.preventDefault()
+  e.preventDefault();
+  const { base_url, query, search } = this.state;
+  this.setState({
+   url: `${base_url}${query}${search}`,
+   search: ''
+  });
  }
 
  render() {
@@ -49,7 +65,16 @@ export default class Recipes extends Component {
      handleChange={this.handleChange}
      handleSubmit={this.handleSubmit}
     />
-    <RecipeList recipes={this.state.recipes} />
+    {this.state.error ? (<section>
+     <div className="row">
+      <div className="col">
+       <h2 className="text-orange text-center text-uppercase mt-5">
+        {this.state.error}
+       </h2>
+      </div>
+     </div>
+    </section>) : (<RecipeList recipes={this.state.recipes} />)}
+
    </>
   )
  }
